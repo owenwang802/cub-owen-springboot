a9b446ba7e899ad96db8cc525e40047a8e202037
owenwang802 <yunl34646@gmail.com>
2025-05-24 20:16:44 +0800

    init

---
+++ cub-owen-springboot/src/main/java/com/owen/cubowenspringboot/CubOwenSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.owen.cubowenspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubOwenSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CubOwenSpringbootApplication.class, args);
+    }
+
+}
---
+++ cub-owen-springboot/src/test/java/com/owen/cubowenspringboot/CubOwenSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owen.cubowenspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubOwenSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
